8b96dfdf173c4b9946dee65388b5028784302657
김승환 <prpn97@gmail.com>
2023-07-07 22:24:56 +0900

    Feat : mysql db연결

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ import session from 'express-session';
 import cors from 'cors';
 import routes from './routes.js';
 import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
 const app = express();
 // env
 dotenv.config();
 const port = process.env.PORT || 3000;
 export const key = process.env.KEY;
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env
+//db 연결
+export let db;
+export const dbLoader = async () => {
+if(db){
+  return db;
+}
+  try {
+    db = await mysql.createPool({
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
+    console.log('Cloud SQL server connection successful');
+    return db;
+  } catch (error) {
+    console.error('Failed to establish connection to the Cloud SQL server:', error);
+    throw error;
+  }
+};
+
+async function startServer(){
+  try{
+    await dbLoader();
+
 
 // session 설정
 app.use(
@@ -20,21 +50,28 @@ app.use(
 );
 
 // 미들웨어 및 라우터 설정
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://127.0.0.1:60783',
+    origin: 'http://127.0.0.1:55597',
     methods: ['GET', 'POST', 'OPTIONS'],
     credentials: true,
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 // 라우터 연결
 app.use('/', routes);
+app.set(db)
 
 // 서버 시작
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+  }catch(error){
+    console.error('Failed to start the server:', error);
+  }
+}
+startServer();
--- routes.js
+++ routes.js
@@ -1,40 +1,42 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { key } from './index.js';
-
+import { authenticateUser } from './services.js'
 const router = express.Router();
 
 // 로그인
-router.post('/session_login', (req, res) => {
+router.post('/session_login',async (req, res) => {
   // 사용자 인증 로직 구현
   const { username, password } = req.body;
   console.log(username);
   console.log(password);
-
-  // 사용자 인증 성공
-  if (username === 'admin' && password === 'password') {
-    req.session.authenticated = true; // 세션에 인증 정보 저장
+try{  // 사용자 인증 성공
+  const authenticated = await authenticateUser(username,password)
+  if (authenticated) {
+    req.session.authenticateUser = true; // 세션에 인증 정보 저장
     req.session.user = { username }; // 세션에 사용자 정보 저장
     res.send('세션 로그인 성공');
-  } else {
+  } }catch(error){
     res.status(401).send('세션 로그인 실패');
+    console.error('error message : ',error)
   }
 });
 
-router.post('/token_login', (req, res) => {
+router.post('/token_login', async (req, res) => {
   // 사용자 인증 로직 구현
   const { username, password } = req.body;
   console.log(username);
   console.log(password);
-
+try{
   // 사용자 인증 성공
-  if (username === 'admin' && password === 'password') {
+  const authenticated = await authenticateUser(username,password)
+  if (authenticated) {
     // 토큰 생성 및 반환
     const token = generateToken(username);
     res.send({ message: "토큰 로그인 성공",token });
-  } else {
-    // 인증 실패 처리
+  }} catch(error){
     res.status(401).send('토큰 로그인 실패');
+    console.error('error message: ',error)
   }
 });
 
