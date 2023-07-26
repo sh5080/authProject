5baab44dd6188fba34ed4f24cd8ec1edf5702dfa
김승환 <prpn97@gmail.com>
2023-07-26 20:04:43 +0900

    feat: 멱등성 보장하는 경우 추가

--- src/middlewares/checkIdempotency.js
+++ src/middlewares/checkIdempotency.js
@@ -1,17 +1,22 @@
-const previousResults = {};
+const idempotencyCache = {};
 
 export function checkIdempotency(req, res, next) {
   const { url, method, body } = req;
-  const key = `${method}:${url}:${JSON.stringify(body)}`;
 
-  // 이전 결과와 현재 요청 결과를 비교
-  if (previousResults[key] !== undefined) {
-    // 멱등성이 깨짐을 로그로 출력하거나 처리
-    console.log(`멱등성이 깨짐: ${method} ${url}`);
+  // 멱등성 검사 대상이 되는 API 호출 (예: POST, PUT, DELETE)만을 대상으로 합니다.
+  if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
+    const key = `${method}:${url}:${JSON.stringify(body)}`;
+
+    if (idempotencyCache[key] !== undefined) {
+      console.log(`멱등성이 깨짐: ${method} ${url}`);
+    } else {
+      console.log(`멱등성을 보장함: ${method} ${url}`);
+    }
+
+    // 이전 결과를 삭제하여, 새로운 요청 결과만을 기록합니다.
+    idempotencyCache[key] = true;
   }
 
-  // 현재 요청 결과를 이전 결과에 저장
-  previousResults[key] = true;
-
+  console.log('현재 캐시: ', idempotencyCache);
   next();
 }
