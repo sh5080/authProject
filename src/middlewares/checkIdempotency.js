2d146f027c2d8ed6e93a1a89a13440bcff72c8f0
김승환 <prpn97@gmail.com>
2023-07-28 22:12:54 +0900

    feat: 요청하는 시점, 라우터에서 받아서 해당 미들웨어에서 검사 후 전송

--- src/middlewares/checkIdempotency.js
+++ src/middlewares/checkIdempotency.js
@@ -6,21 +6,24 @@ export async function saveRequestToDB(req, res, next) {
   try {
     const db = await dbLoader();
     const key = req.sessionID;
-    console.log('1: ', key);
     res.cookie('iKey', key, {
       httpOnly: true,
       maxAge: 3600000, //1시간
     });
+    const exceptPassword = { ...req.body };
+    delete exceptPassword.password;
     const requestData = {
       method: req.method,
       url: req.url,
-      body: JSON.stringify(req.body),
+      body: JSON.stringify(exceptPassword) || null,
+      cookie: JSON.stringify(req.cookies) || null,
       key: key,
     };
-
+    // console.log('1: ', req.cookies[0]);
+    // console.log('2: ', req.cookies);
     await db.execute(
-      'INSERT INTO requests (method, url, body, `key`) VALUES (?, ?, ?, ?)',
-      [requestData.method, requestData.url, requestData.body, requestData.key],
+      'INSERT INTO requests (method, url, body, cookie,`key`) VALUES (?, ?, ?, ?, ?)',
+      [requestData.method, requestData.url, requestData.body, requestData.cookie, requestData.key],
 
       console.log('Request data saved to DB:', requestData)
     );
@@ -34,24 +37,48 @@ export async function saveRequestToDB(req, res, next) {
 // 라우터 후에 DB에 저장한 req와 현재 req를 비교하는 미들웨어
 export async function checkIdempotency(req, res, next) {
   const sessionID = req.sessionID;
-  console.log('2:', sessionID);
+
   try {
     // 이전 요청 데이터를 DB에서 조회
     const previousRequests = await getRequestsFromDB(sessionID);
-    // console.log('여기: ', previousRequests);
     const previousRequest = previousRequests.find(
-      (request) => request.method === req.method && request.url === req.url
+      (request) => request.method === req.method && request.url === req.url && request.key === req.sessionID
     );
+    const { request_id, ...previous } = previousRequest;
+    const previousData = {
+      ...previous,
+      body: JSON.stringify(previous.body),
+    };
 
+    const exceptPassword = { ...req.body };
+    delete exceptPassword.password;
+    const token = res.locals.responseData.token;
+    if (token) {
+      req.cookies.tokenID = token;
+    }
+    const current = {
+      method: req.method,
+      url: req.url,
+      body: JSON.stringify(exceptPassword),
+      cookie: JSON.stringify(req.cookies),
+      key: req.sessionID,
+    };
+    // console.log('1!: ', previousRequest);
+    const previousJSON = JSON.stringify(previousData);
+    const currentJSON = JSON.stringify(current);
     if (previousRequest) {
       // 이전 요청과 현재 요청을 비교하여 멱등성 검사
-      if (previousRequest.affectedRows > 0) {
+      if (previousJSON === currentJSON) {
         // 같은 요청이면 멱등하다고 판단
         console.log('멱등성을 만족합니다.');
+        console.log('이전 요청: ', previousJSON);
+        console.log('현재 요청: ', currentJSON);
+        console.log('쿠키?: ', req.cookies);
       } else {
         // 다른 요청이면 멱등하지 않다고 판단
-        console.log('멱등성이 깨졌습니다. 이전 요청: ', previousRequests);
-        console.log('현재 요청: ', previousRequest);
+        console.log('멱등성이 깨졌습니다.');
+        console.log('이전 요청: ', previousJSON);
+        console.log('현재 요청: ', currentJSON);
       }
     } else {
       // 이전 요청이 없는 경우
