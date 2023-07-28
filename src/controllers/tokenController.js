64aec77b86df1e1a2594e63d6c40994dd97e209e
김승환 <prpn97@gmail.com>
2023-07-28 22:11:43 +0900

    fix: 해쉬된 비밀번호 비교, 멱등성 검사 위해 응답값 다음 미들웨어로 전송

--- src/controllers/tokenController.js
+++ src/controllers/tokenController.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { key } from '../index.js';
 import { authenticateUser } from '../services/services.js';
 import { AppError, CommonError } from '../middlewares/errorHandler.js';
-
+import bcrypt from 'bcrypt';
 export function generateToken(username) {
   // 토큰 생성
   const token = jwt.sign({ username }, key, { expiresIn: '1h' });
@@ -14,15 +14,21 @@ export async function login(req, res, next) {
   const { username, password } = req.body;
   try {
     // 사용자 인증 성공
-    const authenticated = await authenticateUser(username, password);
+    const authenticated = await authenticateUser(username);
+    if (!authenticated) {
+      throw new AppError(CommonError.INVALID_INPUT, '없는 id이거나 잘못된 비밀번호입니다.', 401);
+    }
+    const isPasswordMatch = await bcrypt.compare(password, authenticated.password);
+    if (!isPasswordMatch) {
+      throw new AppError(CommonError.AUTHENTICATION_ERROR, '없는 id이거나 잘못된 비밀번호입니다.', 401);
+    }
     if (authenticated) {
       // 토큰 생성 및 반환
       const token = generateToken(username);
-      res.cookie('tokenID', token, { httpOnly: true, maxAge: 3600000 });
-      res.send({ message: `${username} 님 환영합니다.`, token });
-    }
-    if (!authenticated) {
-      throw new AppError(CommonError.INVALID_INPUT, '없는 id이거나 잘못된 비밀번호입니다.', 401);
+      res.cookie('tokenID', token, { httpOnly: true, maxAge: 3600000 }); //1시간
+      // res.send({ message: `${username} 님 환영합니다.`, token });
+      res.locals.responseData = { message: `${username} 님 환영합니다.`, token };
+      next();
     }
   } catch (error) {
     console.error(error);
@@ -35,6 +41,8 @@ export async function logout(req, res) {
   res.send('토큰 로그아웃 성공');
 }
 
-export async function checkToken(req, res) {
-  res.json(`토큰이 유효합니다. ${req.username} 으로 로그인된 상태입니다.`);
+export async function checkToken(req, res, next) {
+  // res.json(`토큰이 유효합니다. ${req.username} 으로 로그인된 상태입니다.`);
+  res.locals.responseData = `토큰이 유효합니다. ${req.username} 으로 로그인된 상태입니다.`;
+  next();
 }
